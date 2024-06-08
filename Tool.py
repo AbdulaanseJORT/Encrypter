19453679d3c724e2abbdf3d5854929f2c1ee09f6
Jort <122091817+AbdulaanseJORT@users.noreply.github.com>
2024-06-08 15:50:14 +0200

    Update Tool.py

--- Tool.py
+++ Tool.py
@@ -6,31 +6,16 @@ import os
 class encryption:
     def __init__(self):
         key = Fernet.generate_key()
+        os.system("cls")
         input_text = input("Enter the text you want to encrypt: ")
         input_text = input_text.encode()
         f = Fernet(key)
         encrypted_text = f.encrypt(input_text)
-        print("The encrypted text is: ", encrypted_text)
-
-
-class decryption:
-    def __init__(self):
-        key = Fernet.generate_key()
-        input_text = input("Enter the text you want to decrypt: ")
-        input_text = input_text.encode()
-        f = Fernet(key)
-        decrypted_text = f.decrypt(input_text)
-        print("The decrypted text is: ", decrypted_text.decode())
-    
+        print("Here is your encrypted text: ", encrypted_text)
 
 class tool:
-    choice = input("Do you want to encrypt or decrypt? (E/D): ")
-    if choice == "E":
-        encryption()
-    elif choice == "D":
-        decryption()
-    else:
-        print("Invalid choice.")
+    input("Welcome to the tool! Please press enter to continue.")
+    encryption()
 
 while True:
     tool()
